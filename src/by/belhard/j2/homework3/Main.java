b87a887487b1a6436fb11188a202672f6150b2b1
Galdava <alexgaldava@gmail.com>
2019-04-24 02:26:26 +0300

    home1

--- src/by/belhard/j2/homework3/Main.java
+++ src/by/belhard/j2/homework3/Main.java
@@ -1,13 +1,56 @@
 package by.belhard.j2.homework3;
 
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        Market x = new Market();
+        Map<String,Product > product =x.getProduct();
 
+        product.put("prod1", new Prod("prod1", 10));
+        product.put("prom1", new Prom("prom1", 20));
+        product.put("forA1", new ForAnimals("forA1", 15));
+        product.put("prod2", new Prod("prod2", 30));
 
+        Map<String, Product> cart = x.getCart();
 
+        cart.put("cart1", product.get("prom1"));
+        cart.put("cart2", product.get("prom1"));
+        cart.put("cart3", product.get("prod1"));
+        System.out.println(cart.values());
+        double sum = 0;
+        for (Map.Entry<String, Product> entry : cart.entrySet())
+            sum += entry.getValue().getPrice();
+        System.out.println("Full Price=" + sum);
+
+//        product.put("prod1", new Prod("prod1", 10));
+//        product.put("prom1", new Prom("prom1", 20));
+//        product.put("forA1", new ForAnimals("forA1", 15));
+//        product.put("prod2", new Prod("prod2", 30));
     }
+//    Market x = new Market();
+//    public void addTo(){
+//        HashMap<String,Product> product =x.getProduct();
+//        product.put("prod1", new Prod("prod1", 10));
+//        product.put("prom1", new Prom("prom1", 20));
+//        product.put("forA1", new ForAnimals("forA1", 15));
+//        product.put("prod2", new Prod("prod2", 30));
+//
+//        HashMap<String, Product> cart = x.getCart();
+//
+//        cart.put("cart1", product.get("prom1"));
+//        cart.put("cart2", product.get("prom1"));
+//        cart.put("cart3", product.get("prod1"));
+//        System.out.println(cart.values());
+
+
+
+
+
+
+    //}
 
 
 
--- src/by/belhard/j2/homework3/Market.java
+++ src/by/belhard/j2/homework3/Market.java
@@ -7,23 +7,39 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class Market {
-    public static void main(String[] args) {
-        Map<String, Product> product = new HashMap<>();
-        product.put("prod1", new Prod("prod1", 10));
-        product.put("prom1", new Prom("prom1", 20));
-        product.put("forA1", new ForAnimals("forA1", 15));
-        product.put("prod2", new Prod("prod2", 30));
+    private Map<String,Product> product = new HashMap<>();
+    private Map<String,Product> cart = new HashMap<>();
+
+    public Map<String, Product> getProduct() {
+        return product;
+    }
+
+    public Map<String, Product> getCart() {
+        return cart;
+    }
 
 
-        Map<String, Product> cart = new HashMap<>();
-        cart.put("cart1", product.get("prom1"));
-        cart.put("cart2", product.get("prom1"));
-        cart.put("cart3", product.get("prod1"));
-        System.out.println(cart.values());
-        double sum = 0;
-        for (Map.Entry<String, Product> entry : cart.entrySet())
-            sum += entry.getValue().getPrice();
-        System.out.println("full Price=" + sum);
+
+}
+    //    public void AddToP(String name, Product product,) {
+//        Map<String, Product> product = new HashMap<>();
+//        product.put("name", new Prod("name", x));
+////        product.put("prod1", new Prod("prod1", 10));
+////        product.put("prom1", new Prom("prom1", 20));
+////        product.put("forA1", new ForAnimals("forA1", 15));
+////        product.put("prod2", new Prod("prod2", 30));
+//    }
+//
+//    public void AddToCart(){
+//        Map<String, Product> cart = new HashMap<>();
+//        cart.put("cart1", product.get("prom1"));
+//        cart.put("cart2", product.get("prom1"));
+//        cart.put("cart3", product.get("prod1"));
+//        System.out.println(cart.values());
+//        double sum = 0;
+//        for (Map.Entry<String, Product> entry : cart.entrySet())
+//            sum += entry.getValue().getPrice();
+//        System.out.println("Full Price=" + sum);
 
 
 //        Iterator  <Map.Entry<String ,Product> > itr = cart.entrySet().iterator();
@@ -31,7 +47,7 @@ public class Market {
 //                System.out.println("Full price="+(itr.next().getValue().getPrice()+ itr.next().getValue().getPrice()));
 
 
-    }
+   // }
 
 
-}
+//}
